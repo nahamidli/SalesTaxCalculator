7274a4c32682c530e1210d5cc78fdb4c945f879b
Nahid <nahid.hemid@gmail.com>
2023-04-19 02:24:29 +0200

    - Removed unnecessary println

--- src/main/java/com/interview/salestaxcalculator/Main.java
+++ src/main/java/com/interview/salestaxcalculator/Main.java
@@ -2,7 +2,6 @@ package com.interview.salestaxcalculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         // Create an instance of SalesTaxCalculator and call its main method
         SalesTaxCalculatorApp salesTaxCalculator = new SalesTaxCalculatorApp();
